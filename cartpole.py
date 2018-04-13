18fb011363ec3be6c82cddd0c18c46d5369c3e63
Daniel Dsouza <ddsouza@umich.edu>
2018-04-13 12:48:23 -0400

    - Added cartpole.py to Master - Initial Code to simply run and see the Cartpole Environment - Code to see run a new environment after "done"==TRUE. Repeated 20 times - Implemented Random Search for 100k trails. Got a Max Reward of 100

--- cartpole.py
+++ cartpole.py
@@ -2,18 +2,37 @@
 
 # Libraries
 import gym
-
+import numpy as np
 # Create Environments
 env = gym.make('CartPole-v0')
 
-for i_episode in range(20):
+
+bestParams = None
+bestreward = 0
+for i_episode in range(100000):
     observation = env.reset()
+    parameters = (np.random.rand(4)*2) - 1
+    total_reward = 0
     for t in range(100):
         env.render()
-        print(observation)
-        action = env.action_space.sample()
+        # print(observation)
+        action = 0 if np.matmul(observation,parameters)<0 else 1
         observation, reward, done, info = env.step(action)
+        total_reward+=reward
         if done:
-            print("Episode finished after {} timesteps".format(t + 1))
+            print("Episode :",i_episode,"\tTotal_Reward  = ",total_reward)
+            # print("Episode finished after {} timesteps".format(t + 1))
             break
+    if total_reward > bestreward:
+        bestParams = parameters
+        bestreward = total_reward
+    if total_reward > 200:
+        print(" Stabilized")
+        print(" Good Weights are : ", parameters)
+        break
+print(" The Best Params were :",bestParams," with Reward :",bestreward)
 
+#
+# print(env.action_space)
+#
+# print(env.observation_space)
