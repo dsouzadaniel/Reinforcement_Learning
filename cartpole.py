2a2718c3d712bde1309e01cc219d3fcf006b4c9a
Daniel Dsouza <ddsouza@umich.edu>
2018-04-13 13:28:47 -0400

    - Added cartpole.py to Master - Initial Code to simply run and see the Cartpole Environment - Code to see run a new environment after "done"==TRUE. Repeated 20 times - Implemented Random Search for 100k trails. Got a Max Reward of 100 - Implemented Hill Climb Algorithm. Achieved Max Reward of 200.

--- cartpole.py
+++ cartpole.py
@@ -6,17 +6,17 @@ import numpy as np
 # Create Environments
 env = gym.make('CartPole-v0')
 
-
-bestParams = None
 bestreward = 0
-for i_episode in range(100000):
+rando_noise_scaling = 0.1
+parameters = (np.random.rand(4)*2) - 1
+for i_episode in range(1000):
     observation = env.reset()
-    parameters = (np.random.rand(4)*2) - 1
+    new_parameters = parameters + (((np.random.rand(4)*2) - 1)*rando_noise_scaling)
     total_reward = 0
-    for t in range(100):
+    for t in range(200):
         env.render()
         # print(observation)
-        action = 0 if np.matmul(observation,parameters)<0 else 1
+        action = 0 if np.matmul(observation,new_parameters)<0 else 1
         observation, reward, done, info = env.step(action)
         total_reward+=reward
         if done:
@@ -24,15 +24,16 @@ for i_episode in range(100000):
             # print("Episode finished after {} timesteps".format(t + 1))
             break
     if total_reward > bestreward:
-        bestParams = parameters
+        parameters = new_parameters
         bestreward = total_reward
-    if total_reward > 200:
+    if total_reward == 200:
         print(" Stabilized")
         print(" Good Weights are : ", parameters)
         break
-print(" The Best Params were :",bestParams," with Reward :",bestreward)
+print(" The Bests Reward :",bestreward)
 
 #
 # print(env.action_space)
 #
 # print(env.observation_space)
+
